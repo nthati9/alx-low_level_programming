4d2d97346526aa3c46577561f51332294ea5ec74
nthati9 <ntahti9@icloud.com>
2023-10-21 00:54:16 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - using printf in my program
+ *Return: we returing 0 again
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
