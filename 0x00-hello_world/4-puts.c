7895f6485324d941c303a2c63aff65c3d1aa6b5f
nthati9 <ntahti9@icloud.com>
2023-10-21 00:50:27 -0700

    number 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - my main fuction that prints all outputs
+ *
+ * Return: returning O
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
